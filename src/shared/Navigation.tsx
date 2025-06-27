94524a6119dd9779559ee64af10c737a50142a1d
ludomydla <lmmydla@gmail.com>
2025-06-27 21:50:25 +0200

    feat: creating scaffolding, starting with Navigation

---
+++ src/shared/Navigation.tsx
@@ -0,0 +1,14 @@
+export const Navigation = () => {
+
+    return (
+        <nav>
+            <a href="/">
+                <h2>Ludo's home page</h2>
+            </a>
+            <ul>
+                <li><a href="">Projects</a></li>
+                <li><a href="">About</a></li>
+            </ul>
+        </nav>
+    )
+}
